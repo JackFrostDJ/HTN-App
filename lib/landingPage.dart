b6ee2600ec46bf4a58e3603c4f43d9a073004bbc
Rocky <me.rocky.sun@gmail.com>
2021-01-17 04:17:30 -0500

    forgot one file

---
+++ lib/landingPage.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'AppColors.dart';
+import 'main.dart';
+import 'model.dart';
+import 'getUserPrefPage.dart';
+import 'result.dart';
+
+AnimationController _animationController;
+
+class FirstRoute extends StatefulWidget {
+  @override
+  _FirstRouteState createState() => _FirstRouteState();
+}
+
+class _FirstRouteState extends State<FirstRoute>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    Timer(Duration(milliseconds: 200), () => _animationController.forward());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.red,
+      // appBar: AppBar(
+      //  title: Text("Testing Testing"),
+      //   backgroundColor: AppColors.blue,
+      // ),
+      body: SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(0, 0.25),
+          end: Offset.zero,
+        ).animate(_animationController),
+        child: FadeTransition(
+          opacity: _animationController,
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30.0, 150.0, 30.0, 0.0),
+                      alignment: Alignment.center,
+                      child: NameForm(), //Text("1 QUESTION",
+                      //   style: TextStyle(
+                      //     fontSize: 24,
+                      //     color: AppColors.white,
+                      //   ),
+                      // ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
+
+class NameForm extends StatefulWidget {
+  @override
+  _NameForm createState() => _NameForm();
+}
+
+class _NameForm extends State<NameForm> {
+  final _formKey = GlobalKey<FormState>();
+  Model model = Model();
+
+  @override
+  Widget build(BuildContext context) {
+    final halfMediaWidth = MediaQuery.of(context).size.width / 1.5;
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 40.0, bottom: 40.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Why Don\'t you introduce yourself!',
+                maxLines: 3,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    color: Colors.blueGrey[100],
+                    fontWeight: FontWeight.w900,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: 'Open Sans',
+                    fontSize: 30),
+              ),
+            ),
+          ),
+          Container(
+            width: halfMediaWidth,
+            decoration: new BoxDecoration(
+              color: AppColors.white,
+              borderRadius: new BorderRadius.all(new Radius.circular(25.7)),
+            ),
+            child: MyFormField(
+              hintText: "First Name",
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Enter your first name";
+                }
+                return null;
+              },
+              onSaved: (String value) {
+                model.firstName = value;
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 0.0),
+            width: halfMediaWidth,
+            decoration: new BoxDecoration(
+              color: AppColors.white,
+              borderRadius: new BorderRadius.all(new Radius.circular(25.7)),
+            ),
+            child: MyFormField(
+              hintText: "Last Name",
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Enter your last name";
+                }
+                return null;
+              },
+              onSaved: (String value) {
+                model.lastName = value;
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 24.0),
+            child: Center(
+              child: FloatingActionButton.extended(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    );
+                  }
+                },
+                label: Text('Next'),
+                icon: Icon(Icons.arrow_forward_rounded),
+                backgroundColor: AppColors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
